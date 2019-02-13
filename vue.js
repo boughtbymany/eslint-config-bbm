f2f887fe81fceb9266688d24009f781b1b490dc1
Andy Pattenden <andyp@boughtbymany.com>
2019-02-13 15:42:14 +0000

    Vue Config
    - Updates Vue config to match our standards.

--- vue.js
+++ vue.js
@@ -1,3 +1,6 @@
+/*
+ * Rules for Vue can be found at https://github.com/vuejs/eslint-plugin-vue/tree/master/docs/rules
+ */
 module.exports = {
   plugins: ["vue"],
   extends: [
@@ -6,9 +9,16 @@ module.exports = {
     "@vue/prettier",
   ],
   rules: {
-    "vue/v-bind-style": ["error", "longform"],
-    "vue/v-on-style": ["error", "longform"],
-    "vue/name-property-casing": ["error", "kebab-case"]
+    // Priority B: Strongly Recommended (Improving Readability)
+    "vue/singleline-html-element-content-newline": "off", // override for headers with dots
+    "vue/attribute-hyphenation": "off",
+    "vue/name-property-casing": "off", // override for Mutt widgets
+    "vue/require-default-prop": "off",
+    "vue/v-bind-style": ["error", "longform"], // override fdefault
+    "vue/v-on-style": ["error", "longform"], // override default
+
+    // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    "vue/no-v-html": "off", // override for translation
   },
   parserOptions: {
     parser: "babel-eslint",
