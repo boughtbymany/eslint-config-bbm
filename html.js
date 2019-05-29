d541f693954920df81363800896a142cd26e0e30
Andy Pattenden <andyp@boughtbymany.com>
2019-05-29 10:08:11 +0100

    Multiple Configurations (#1)
    * Multiple Configurations
    
    - Adds .eslintignore file.
    - Adds eslint config files for different uses.
    - Updates dev dependencies.
    - Updates dependencies to peer dependencies and adds new dependencies.
    - Updates editorconfig with correct indent size.
    - Updates repo's eslint config to point to new files.
    - Updates remark config to remove rule overrides,
    - Updates README with new config options.
    - Removes yarn references from repo.

---
+++ html.js
@@ -0,0 +1,15 @@
+
+/**
+ *  Allows linting and fixing inline scripts contained in HTML files
+ */
+module.exports = {
+  plugins: ["html"],
+  extends: ["@boughtbymany/eslint-config-bbm"],
+  settings: {
+    /*
+     * Override the default extensions for the HTML plugin so that it
+     * doesn't try to handle Vue files – this is handled by the Vue plugin.
+     */
+    'html/html-extensions': [ '.html' ],
+  },
+}
---
+++ jest.js
@@ -0,0 +1,7 @@
+/**
+ *  Allows linting and fixing Jest test scripts
+ */
+module.exports = {
+  plugins: ["jest"],
+  extends: ["@boughtbymany/bbm", "plugin:jest/recommended"],
+}
