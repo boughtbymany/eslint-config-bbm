aca149a78ac42aadb938b1b37a7b3abb9d459550
Gavyn McKenzie <gavyn@etchapps.com>
2020-09-08 14:57:09 +0100

    refactor: remove vue a11y plugin

--- vue.js
+++ vue.js
@@ -2,13 +2,8 @@
  * Rules for Vue can be found at https://github.com/vuejs/eslint-plugin-vue/tree/master/docs/rules
  */
 module.exports = {
-  plugins: ['vue', 'vue-a11y'],
-  extends: [
-    './index.js',
-    'plugin:vue/recommended',
-    '@vue/prettier',
-    'plugin:vue-a11y/base',
-  ],
+  plugins: ['vue'],
+  extends: ['./index.js', 'plugin:vue/recommended', '@vue/prettier'],
   rules: {
     // Priority B: Strongly Recommended (Improving Readability)
     'vue/singleline-html-element-content-newline': 'off', // override for headers with dots
@@ -16,15 +11,6 @@ module.exports = {
 
     // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
     'vue/no-v-html': 'off', // override for translation
-    'vue-a11y/label-has-for': [
-      2,
-      {
-        required: {
-          // Only require one of nesting _or_ id.
-          some: ['nesting', 'id'],
-        },
-      },
-    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
