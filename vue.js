0d7d662687017e685fff17b8178c87c8746d17a2
Gavyn McKenzie <gavyn@etchapps.com>
2020-09-08 15:06:50 +0100

    refactor: keep the plugin, but remove the breaking rule

--- vue.js
+++ vue.js
@@ -2,8 +2,13 @@
  * Rules for Vue can be found at https://github.com/vuejs/eslint-plugin-vue/tree/master/docs/rules
  */
 module.exports = {
-  plugins: ['vue'],
-  extends: ['./index.js', 'plugin:vue/recommended', '@vue/prettier'],
+  plugins: ['vue', 'vue-a11y'],
+  extends: [
+    './index.js',
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    'plugin:vue-a11y/base',
+  ],
   rules: {
     // Priority B: Strongly Recommended (Improving Readability)
     'vue/singleline-html-element-content-newline': 'off', // override for headers with dots
@@ -11,6 +16,16 @@ module.exports = {
 
     // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
     'vue/no-v-html': 'off', // override for translation
+    'vue-a11y/label-has-for': [
+      2,
+      {
+        required: {
+          // Only require one of nesting _or_ id.
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'vue-a11y/click-events-have-key-events': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
