094ffffebe8dd92dc9477946d5fc114c897e1205
Gabriel Lundmark <Gabriel.lundmark@boughtbymany.com>
2021-01-28 10:34:44 +0100

    feat: enable linting for childless tags (#19)
    * feat: enable linting for childless tags
    
    * chore: bump version to 4.1.0

--- vue.js
+++ vue.js
@@ -8,9 +8,17 @@ module.exports = {
     // Priority B: Strongly Recommended (Improving Readability)
     'vue/singleline-html-element-content-newline': 'off', // override for headers with dots
     'vue/name-property-casing': 'off', // override for Mutt widgets
-
     // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
     'vue/no-v-html': 'off', // override for translation
+    // Override self-closing settings by eslint-config-prettier
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
     // A11y rules taken from https://github.com/maranran/eslint-plugin-vue-a11y/blob/master/lib/configs/recommended.js
     // But switched to warn
     'vue-a11y/accessible-emoji': 'warn',
